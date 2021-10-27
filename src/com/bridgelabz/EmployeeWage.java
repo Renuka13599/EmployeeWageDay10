8cca3fd3b25da2f39dc4455bf357097c4c90f724
renuka13599 <renuka13599@gmail.com>
2021-10-27 12:11:49 +0530

    Added:Calculating part time wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,18 +1,27 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    public static final double IS_FULL_TIME=8;
-    public static final double EMP_WAGE_PER_HR=20;
-    public static void main(String[] args)
-    {
-        double Empcheck = (Math.random() * 10) %2 ;
-        if(Empcheck == IS_FULL_TIME)
-            System.out.println("Employee is present");
-        else
-            System.out.println("Employee is absent");
-        double calcDailyWage=0;
-        calcDailyWage=IS_FULL_TIME*EMP_WAGE_PER_HR;
-        System.out.println("Calculate daily wage:" +calcDailyWage);
+    public static double IS_FULL_TIME = 2;
+    public static double IS_PART_TIME = 1;
+   public static double EMP_WAGE_PER_HR = 20;
 
+    public static void main(String[] args) {
+        int empHrs = 0;
+        int empWage = 0;
+        //Computation
+        double empCheck = Math.floor(Math.Random() * 10) % 3;
+        if (empCheck == IS_PART_TIME)
+            empHrs = 4;
+        else if (empCheck == IS_FULL_TIME)
+            empHrs = 8;
+        else
+            empHrs = 0;
+        empWage = empHrs * EMP_RATE_PER_HR;
+        System.out.println("Emp Wage:" + empWage);
     }
 }
+
+
+
+
+
