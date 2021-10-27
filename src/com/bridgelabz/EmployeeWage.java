3fd6852d357269a043b801c1fa306c286713d6ff
renuka13599 <renuka13599@gmail.com>
2021-10-27 12:00:20 +0530

    Added:Calculating daily wages

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,16 +1,18 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    public static final double IS_FULL_TIME = 1;
-
-    public static void main(String[] args) {
-        double IS_FULL_TIME = 1;
-        double Empcheck = (Math.random() * 10) % 2;
-        if (Empcheck == IS_FULL_TIME)
+    public static final double IS_FULL_TIME=8;
+    public static final double EMP_WAGE_PER_HR=20;
+    public static void main(String[] args)
+    {
+        double Empcheck = (Math.random() * 10) %2 ;
+        if(Empcheck == IS_FULL_TIME)
             System.out.println("Employee is present");
         else
             System.out.println("Employee is absent");
+        double calcDailyWage=0;
+        calcDailyWage=IS_FULL_TIME*EMP_WAGE_PER_HR;
+        System.out.println("Calculate daily wage:" +calcDailyWage);
+
     }
 }
-
-
